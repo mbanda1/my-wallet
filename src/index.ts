a30fc2142b9f3a5529318756e8efe9c2edcb5db1
Nixon <34273829+mbanda1@users.noreply.github.com>
2024-03-21 19:42:06 +0300

    Initialize DB

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import express, { Express, Request, Response } from "express";
-import mongooseDbRun from "./mongoose";
+import express, { Express, NextFunction, Request, Response } from "express";
+import mongooseDb from "./mongoose";
+import transactionRoutes from './routes//transaction';
+import walletRoutes from './routes/wallet';
 
 const app: Express = express();
 
+app.use(express.json());
+app.use('/api/wallet', walletRoutes);
+app.use('/api/transaction', transactionRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Live server");
 });
 
-mongooseDbRun()
+mongooseDb()
+
+// error middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong');
+});
 
 app.listen(3001, () => {
   console.log(`[server]: Server is running at http://localhost:3001`);
