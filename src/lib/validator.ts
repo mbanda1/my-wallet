640b6ab4ed547914f7d127a9491a5a0ccdb4b932
Nixon <34273829+mbanda1@users.noreply.github.com>
2024-03-22 15:23:12 +0300

    Schema validator

---
+++ src/lib/validator.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+import { APIError } from "./http-error";
+
+export const validateRequestBody = () => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    next()
+  } catch (error) {
+    const err = new APIError({
+      message: 'Data supplied does not match the schema',
+    });
+    return next(err);
+  }
+};
